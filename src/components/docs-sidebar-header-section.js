37bb69c8f077b0454345511894968c0fd51f360d
ShubhanjanMedhi-dev <shubhanjan@squadcast.com>
2022-01-19 17:47:25 +0530

    feat: added squadcast logo

--- src/components/docs-sidebar-header-section.js
+++ src/components/docs-sidebar-header-section.js
@@ -1,20 +1,13 @@
 import React from "react"
 
-import CloudflareLogo from "./cloudflare-logo"
 import DocsNavLogoLockup from "./docs-nav-logo-lockup"
+import SquadcastLogo from "./squadcast-logo"
 
 const DocsSidebarHeaderSection = () => (
   <div className="DocsSidebar--section DocsSidebar--header-section">
     <a className="DocsSidebar--cloudflare-logo-link DocsSidebar--link" href="https://developers.cloudflare.com/">
       <DocsNavLogoLockup
-        logo={<CloudflareLogo/>}
-        text={(
-          <>
-            <span data-text="Cloudflare">Cloudflare</span>
-            <span>&nbsp;</span>
-            <span data-text="Docs">Docs</span>
-          </>
-        )}
+        logo={<SquadcastLogo/>}
       />
     </a>
   </div>
