62e3c40ec54db7dbfab43b1a15a82465a0974cc3
ShubhanjanMedhi-dev <shubhanjan@squadcast.com>
2022-01-20 15:06:39 +0530

    fix:logo

--- src/components/docs-mobile-header.js
+++ src/components/docs-mobile-header.js
@@ -1,14 +1,14 @@
 import React from "react"
 
 import DocsNavLogoLockup from "./docs-nav-logo-lockup"
-import SquadcastLogo from "./squadcast-logo"
+import SquadcastLogoCircle from "./squadcast-logo-circle"
 
 const DocsMobileHeader = () => (
   <div className="DocsMobileHeader">
     <a className="DocsMobileHeader--cloudflare-logo-link Link Link-without-underline" href="https://www.squadcast.com/">
       <DocsNavLogoLockup
         small={true}
-        logo={<SquadcastLogo/>}
+        logo={<SquadcastLogoCircle/>}
       />
     </a>
   </div>
--- src/components/squadcast-logo-circle.js
+++ src/components/squadcast-logo-circle.js
@@ -1,29 +1,32 @@
 import React from "react"
-import AccessibleSVG from "./accessible-svg"
 
 const SquadcastLogoCircle = () => (
-  <AccessibleSVG title="Squadcast Logo" viewBox="0 0 48 48">
-    <defs>
-        <linearGradient x1="11.3497988%" y1="100%" x2="70.781804%" y2="4.97274114%" id="linearGradient-1">
-            <stop stop-color="#1F92FF" offset="0%"></stop>
-            <stop stop-color="#1B7FF1" offset="20.2034776%"></stop>
-            <stop stop-color="#000395" offset="100%"></stop>
-        </linearGradient>
-        <linearGradient x1="0%" y1="87.636094%" x2="76.8502469%" y2="0%" id="linearGradient-2">
-            <stop stop-color="#1F92FF" offset="0%"></stop>
-            <stop stop-color="#1B7FF1" offset="20.2034776%"></stop>
-            <stop stop-color="#000395" offset="100%"></stop>
-        </linearGradient>
-    </defs>
-    <g id="Primary-Logo" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="Group">
-            <g id="Group-Copy">
-                <path d="M152.184473,2.76131825 C156.415819,3.46152057 160.949638,4.45248404 165.847675,5.80430782 C197.577715,14.5615761 229.980005,44.5724903 203.008105,38.5726764 C161.879114,26.1224442 135.038506,50.7931677 121.645893,79.8614688 C121.295077,80.6229046 120.940374,81.4017818 120.583711,82.194751 C104.654313,126.314974 96.6277113,201.902701 40.8986968,190.713318 C-15.5252212,172.764119 -1.8475754,90.4062413 18.0576624,59.5702959 C39.4598702,26.4153384 64.1520099,8.10775406 101.166274,2.1865424 C121.603665,-1.08284903 138.555235,-0.105982834 152.184466,2.76131668 Z" id="Combined-Shape-Copy-6" fill="url(#linearGradient-1)"></path>
-                <path d="M197.373757,54.795453 C201.605103,55.4956554 206.138922,56.4866188 211.036959,57.8384426 C242.766999,66.5957109 275.169289,96.6066251 248.197388,90.6068112 C207.068398,78.156579 180.227789,102.827303 166.835177,131.895604 C166.484361,132.657039 166.129658,133.435917 165.772995,134.228886 C149.843597,178.349109 141.816995,253.936836 86.0879807,242.747453 C29.6640626,224.798254 43.3417085,142.440376 63.2469462,111.604431 C84.6491541,78.4494732 109.341294,60.1418889 146.355558,54.2206772 C166.792949,50.9512858 183.744518,51.928152 197.37375,54.7954515 Z" id="Combined-Shape-Copy-7" fill="url(#linearGradient-2)" transform="translate(152.094642, 148.017067) scale(-1, -1) translate(-152.094642, -148.017067) "></path>
-            </g>
-        </g>
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 48 48" style="enable-background:new 0 0 261 261;" xmlSpace="preserve">
+
+    <g id="Group-Copy">
+	
+		<linearGradient id="Combined-Shape-Copy-6_1_" gradientUnits="userSpaceOnUse" x1="-824.5085" y1="668.3428" x2="-823.9142" y2="669.2931" gradientTransform="matrix(212.3111 0 0 -191.6929 175071.9688 128313.4375)">
+		<stop  offset="0" style="stop-color:#1F92FF"/>
+		<stop  offset="0.202" style="stop-color:#1B7FF1"/>
+		<stop  offset="1" style="stop-color:#000395"/>
+	    </linearGradient>
+	    <path id="Combined-Shape-Copy-6" fill="url(#Combined-Shape-Copy-6_1_)" fill-rule="evenodd" clip-rule="evenodd" d="M153.2,11.3c4.2,0.7,8.8,1.7,13.7,3c31.7,8.8,64.1,38.8,37.2,32.8
+		c-41.1-12.5-68,12.2-81.4,41.3c-0.4,0.8-0.7,1.5-1.1,2.3c-15.9,44.1-24,119.7-79.7,108.5C-14.5,181.3-0.8,98.9,19.1,68.1
+		c21.4-33.2,46.1-51.5,83.1-57.4C122.6,7.4,139.6,8.4,153.2,11.3L153.2,11.3z"/>
+	
+		<linearGradient id="Combined-Shape-Copy-7_1_" fill-rule="evenodd" clip-rule="evenodd" gradientUnits="userSpaceOnUse" x1="-832.4094" y1="672.8011" x2="-831.6409" y2="673.6775" gradientTransform="matrix(-212.3111 0 0 191.6929 -176469.9688 -128886.4375)">
+		<stop  offset="0" style="stop-color:#1F92FF"/>
+		<stop  offset="0.202" style="stop-color:#1B7FF1"/>
+		<stop  offset="1" style="stop-color:#000395"/>
+	    </linearGradient>
+	    <path id="Combined-Shape-Copy-7" fill="url(#Combined-Shape-Copy-7_1_)" fill-rule="evenodd" clip-rule="evenodd" d="M107.8,249.7c-4.2-0.7-8.8-1.7-13.7-3C62.4,237.9,30,207.9,57,213.9
+		c41.1,12.5,68-12.2,81.4-41.3c0.4-0.8,0.7-1.5,1.1-2.3c15.9-44.1,24-119.7,79.7-108.5c56.4,17.9,42.7,100.3,22.8,131.1
+		c-21.4,33.2-46.1,51.5-83.1,57.4C138.4,253.6,121.4,252.6,107.8,249.7L107.8,249.7z"/>
     </g>
-  </AccessibleSVG>
+    </svg>
+
+
 )
 
 export default SquadcastLogoCircle
